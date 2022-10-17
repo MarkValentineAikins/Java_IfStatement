e697cf14b861c374e8f1126fd9e7d9ee3c4c7e40
Mark Valentine Aikins <valentineaikins@icloud.com>
2022-10-17 12:15:56 +0000

    Starting with java if statement

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Decision structure programming");
+
+        double score1;
+        double score2;
+        double score3;
+        double averageScore;
+        String scoreTotal;
+
+        scoreTotal = JOptionPane.showInputDialog("Enter your first score ");
+        score1 = Double.parseDouble(scoreTotal);
+
+        //Get second score
+
+        scoreTotal = JOptionPane.showInputDialog("Enter your second score");
+        score2 = Double.parseDouble(scoreTotal);
+
+        //Get third score
+
+        scoreTotal = JOptionPane.showInputDialog("Enter your third score");
+        score3 = Double.parseDouble(scoreTotal);
+
+        averageScore = (score1 + score2 + score3) /3.0;
+        JOptionPane.showMessageDialog(null, "Your average score is " + averageScore);
+
+        //making decision to determine if average score is greater than or equal to 96
+
+        if (averageScore > 96) {
+            JOptionPane.showMessageDialog(null, "Your average score of " + " " + averageScore +
+                    " is greater than or equal to 96\n " + " " + "Well done");
+        }
+        System.exit(0);
+    }
+}
